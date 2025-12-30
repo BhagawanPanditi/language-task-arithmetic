36ec2c58d6a1d8443b05a224b5d13a58160c4c2e
BhagawanPanditi <bhagawanlalith@gmail.com>
2025-12-30 08:51:35 +0530

    initial setup

---
+++ open-instruct/scripts/custom_train.sh
@@ -0,0 +1,34 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export HF_DATASETS_CACHE=/home/compiling-ganesh/24m0829/language-task-arithmetic/hf_datasets_cache
+export TRANSFORMERS_CACHE=/home/compiling-ganesh/24m0829/language-task-arithmetic/hf_transformers_cache
+export TMPDIR=/home/compiling-ganesh/24m0829/language-task-arithmetic/tmp
+
+accelerate launch \
+    --mixed_precision bf16 \
+    --num_processes 8 \
+    --use_deepspeed \
+    --deepspeed_config_file /home/compiling-ganesh/24m0829/language-task-arithmetic/open-instruct/configs/ds_configs/stage3_no_offloading_accelerate.conf \
+    --deepspeed_multinode_launcher standard \
+    /home/compiling-ganesh/24m0829/language-task-arithmetic/open-instruct/open_instruct/finetune_with_val.py \
+    --model_name_or_path meta-llama/Llama-3.2-3B \
+    --tokenizer_name_or_path meta-llama/Llama-3.2-3B-Instruct \
+    --use_slow_tokenizer \
+    --use_flash_attn \
+    --max_seq_length 4096 \
+    --per_device_train_batch_size 16 \
+    --gradient_accumulation_steps 1 \
+    --learning_rate 5e-06 \
+    --lr_scheduler_type linear \
+    --checkpointing_steps 300 \
+    --eval_steps 200 \
+    --warmup_ratio 0.03 \
+    --weight_decay 0.0 \
+    --num_train_epochs 3 \
+    --output_dir /home/compiling-ganesh/24m0829/language-task-arithmetic/open-instruct/output/llama-sft-xnli-language-compliment/ \
+    --with_tracking \
+    --report_to wandb \
+    --logging_steps 1 \
+    --reduce_loss sum \
+    --model_revision main \
+    --exp_name llama-sft-xnli-language-compliment \
+    --seed 123
