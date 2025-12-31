35feb726ef5f1c273ba066861020722cda6eb04d
Panditi Bhagawan <bhagawanlalith@gmail.com>
2025-12-31 14:43:19 +0530

    prajna update

--- open-instruct/scripts/custom_train.sh
+++ open-instruct/scripts/custom_train.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1
 export TMPDIR=/home/compiling-ganesh/24m0829/language-task-arithmetic/tmp
 
 accelerate launch \
     --mixed_precision bf16 \
-    --num_processes 4 \
+    --num_processes 2 \
     --use_deepspeed \
     --deepspeed_config_file /home/compiling-ganesh/24m0829/language-task-arithmetic/open-instruct/configs/ds_configs/stage3_no_offloading_accelerate.conf \
     --deepspeed_multinode_launcher standard \
@@ -13,11 +13,11 @@ accelerate launch \
     --use_slow_tokenizer \
     --max_seq_length 4096 \
     --per_device_train_batch_size 8 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 8 \
     --learning_rate 5e-06 \
     --lr_scheduler_type linear \
-    --checkpointing_steps 300 \
-    --eval_steps 200 \
+    --checkpointing_steps 100 \
+    --eval_steps 100 \
     --warmup_ratio 0.03 \
     --weight_decay 0.0 \
     --num_train_epochs 3 \
